79a2b946542fe307c11d7f97905095e21f8f3da0
LAPTOP-QC53BQB2\ovand <ovandomunoz@gmail.com>
2022-05-14 04:15:04 -0400

    mi primer commit

---
+++ ApiRest/ApiRest/Controllers/AlumnoController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ApiRest.Models;
+
+namespace ApiRest.Controllers
+{
+    public class AlumnoController : ApiController
+    {
+        private BD_SCHOOLEntities db = new BD_SCHOOLEntities();
+
+        // GET: api/Alumno
+        public IQueryable<Alumno> GetAlumno()
+        {
+            return db.Alumno;
+        }
+
+        // GET: api/Alumno/5
+        [ResponseType(typeof(Alumno))]
+        public IHttpActionResult GetAlumno(int id)
+        {
+            Alumno alumno = db.Alumno.Find(id);
+            if (alumno == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(alumno);
+        }
+
+        // PUT: api/Alumno/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutAlumno(int id, Alumno alumno)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != alumno.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(alumno).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AlumnoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Alumno
+        [ResponseType(typeof(Alumno))]
+        public IHttpActionResult PostAlumno(Alumno alumno)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Alumno.Add(alumno);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = alumno.Id }, alumno);
+        }
+
+        // DELETE: api/Alumno/5
+        [ResponseType(typeof(Alumno))]
+        public IHttpActionResult DeleteAlumno(int id)
+        {
+            Alumno alumno = db.Alumno.Find(id);
+            if (alumno == null)
+            {
+                return NotFound();
+            }
+
+            db.Alumno.Remove(alumno);
+            db.SaveChanges();
+
+            return Ok(alumno);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AlumnoExists(int id)
+        {
+            return db.Alumno.Count(e => e.Id == id) > 0;
+        }
+    }
+}
